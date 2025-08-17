751846ee612d637bf34a74d1f8fa44085f0115cd
Jayantkumar8263 <pjayantk@gmail.com>
2025-08-17 12:33:06 +0530

    17/08/25

--- revision/Function/18.py
+++ revision/Function/18.py
@@ -1,7 +1,7 @@
 '''Write a Python program to execute a string containing Python code.'''
 
 c = 'print("hello world")'
-d = """print("calculations are done")"""
+d = """print("my name is John")"""
 
 exec(c)
 exec(d)
--- revision/Function/Q3.py
+++ revision/Function/Q3.py
@@ -6,6 +6,3 @@ def multiply():
     x = [8, 2, 3, -1, 7]
     print(x[0]*x[1]*x[2]*x[3]*x[4])
 multiply()
-def update():
-    pass
-update()
