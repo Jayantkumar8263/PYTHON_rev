9f0e8216f5b8820774a536c2d3709ea6b62d7044
Jayantkumar8263 <pjayantk@gmail.com>
2025-08-21 14:45:43 +0530

    21.08.25

--- revision/Function/Lambda function Questions/Q4.py
+++ revision/Function/Lambda function Questions/Q4.py
@@ -1,1 +1,16 @@
-''''''
+'''Write a Python program to sort a list of dictionaries using Lambda.
+
+Original list of dictionaries :
+[{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+Sorting the List of dictionaries :
+[{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}]
+
+'''
+
+d = [{'make': 'Nokia', 'model': '216', 'color': 'Black'}, 
+     {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, 
+     {'make': 'Samsung', 'model': '7', 'color': 'Blue'}]
+
+sorted_d = sorted(d, key = lambda d: d['model'])
+print('Orignal list of dictionary:', d)
+print('Sorted List of dictionaries :',sorted_d)
