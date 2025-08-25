e643cb915f12e35e6b51d808d25becc871d7ef13
Jayantkumar8263 <pjayantk@gmail.com>
2025-08-25 12:47:48 +0530

    25/08/25

---
+++ revision/Decorator/Intro.py
@@ -0,0 +1,2 @@
+''' A decorator is essentially a function that takes another function as input, adds some functionality to it, and returns a new function.'''
+
--- revision/Function/Lambda function Questions/Q11.py
+++ revision/Function/Lambda function Questions/Q11.py
@@ -2,3 +2,5 @@
 Original arrays:  [-1, 2, -3, 5, 7, 8, 9, -10]'''
 
 A = [-1, 2, -3, 5, 7, 8, 9, -10]
+sorted_A = sorted(A, key=lambda point : point)
+print(sorted_A)
