ba02f380453d73756c62455a2d2517409c996320
Jayantkumar8263 <pjayantk@gmail.com>
2025-08-26 13:39:25 +0530

    26.08.25

---
+++ revision/Decorator/Q1.py
@@ -0,0 +1,5 @@
+'''1. Create a Decorator to Log Function Arguments and Return Value
+
+Write a Python program to create a decorator that logs the arguments and return value of a function.'''
+
+def Deco_func()
---
+++ revision/Function/function's questions/Intro.py
@@ -0,0 +1,8 @@
+'''functions are the code block which are a set of statements which are executed when we call them.'''
+
+# types of functions :
+# 1. Built in functions 
+# 2. user defined function 
+
+# Advantages :
+'''It helps in reusing the code and saves our time.'''
