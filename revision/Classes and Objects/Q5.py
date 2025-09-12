432ea6b3f81fa2d8d1b5aa31771a57c9cc288144
Jayantkumar8263 <pjayantk@gmail.com>
2025-09-12 22:07:44 +0530

    12/09/25

--- revision/Classes and Objects/Q5.py
+++ revision/Classes and Objects/Q5.py
@@ -6,6 +6,8 @@ class student_info:
     def info(self):
         print(f"{self.name} is in {self.standard}")
 a = student_info()
+#a.name = 'gautam dutta'
+#a.standard = 11
 a.info()
 '''self is a method, argument which we give to the function, which is used to derive it inside a class.'''
 #print(a.name, a.standard, a.age)
